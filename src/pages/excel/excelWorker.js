93fe02caa7d5b6277bbf19c9d22da696e468fca7
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-10-27 23:57:55 +0530

    self removed

--- src/pages/excel/excelWorker.js
+++ src/pages/excel/excelWorker.js
@@ -1,22 +1,16 @@
 /* eslint-env worker */
 import * as XLSX from 'xlsx';
 
-
-self.onmessage = (e) => {
+onmessage = (e) => {
   const file = e.data;
-  const reader = new FileReader();
+  const reader = new FileReaderSync();
+  const data = new Uint8Array(reader.readAsArrayBuffer(file));
+  const workbook = XLSX.read(data, { type: 'array' });
 
-  reader.onload = (event) => {
-    const data = new Uint8Array(event.target.result);
-    const workbook = XLSX.read(data, { type: 'array' });
-    
-    // Extract headers and jsonData
-    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-    const headers = jsonData[0] || [];
+  // Extract headers and jsonData
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+  const headers = jsonData[0] || [];
 
-    self.postMessage({ headers, jsonData });
-  };
-
-  reader.readAsArrayBuffer(file);
+  postMessage({ headers, jsonData });
 };
